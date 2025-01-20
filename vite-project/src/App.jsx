e785976a02eac43280c42606cf81b7436fb14cfc
sonu <sonugupta6045@gmail.com>
2025-01-20 20:41:44 +0530

    Counter part has been done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react"
+
 
 
 function App() {
   
+  let [counter, setCounter] = useState(15)
+  // let counter=5;
 
+  const addvalue =()=>{
+    if(counter < 20) {
+    setCounter(counter+1);
+  }
+};
+  const removeValue = () => {
+    if(counter>10){
+    setCounter(counter-1);
+  }
+}
   return (
    <>
    <h1>Sonu Gupta</h1>
+   <h2>Number Display {counter}</h2>
+  <button onClick={addvalue}>addValue {counter}</button>
+  <br />
+  <button onClick={removeValue}>removeValue {counter}</button>
    </>
   )
 }
