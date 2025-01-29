3da573bd6e8207669fde30a28531b374d8d48cb2
sonu <sonugupta6045@gmail.com>
2025-01-29 22:26:21 +0530

    donw with currency projects

--- currency/src/components/Input.jsx
+++ currency/src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useId} from 'react'
 
 function Input({
     label,
@@ -13,31 +13,39 @@ function Input({
     className = "",
 }) {
    
+    const amountId = useId();
 
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountId} className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
-                    
+                    id={amountId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
+                    disabled={amountDisabled}
+                    value={amount}
+                    onChange={(e)=>onAmountChange && onAmountChange(Number(e.target.value))}
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    
+                    value={selectCurrency}
+                    onChange={(e)=>onCurrencyChange && onCurrencyChange(Number(e.target.value))}
+                    disabled={currencyDisabled}
                 >
                     
-                        <option value="usd">
-                            usd
+                        {currencyOptions.map((currency)=>{
+                            <option key={currency} value={currency}>
+                            {currency}
                         </option>
                 
+                        })}
                 </select>
             </div>
         </div>
